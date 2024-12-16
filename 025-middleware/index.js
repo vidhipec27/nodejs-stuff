99dd8f65d0a53e32f05c3e05f841bf61f3ba8324
vidhipec27 <151131126+vidhipec27@users.noreply.github.com>
2024-12-16 10:12:26 +0000

    learnt about middleware

--- 025-middleware/index.js
+++ 025-middleware/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
 const app = express();
 
+const requestFilter = (request, response, next) => {
+    console.log('request filter');
+    if (!request.query.age)
+        response.send('Please enter an age!');
+    else if (request.query.age < 18)
+        response.send('You may not be able to access this page');
+    else
+        next();
+}
+
+app.use(requestFilter);
+
 app.get('/', (request, response) => {
     response.send('Welcome to home page');
 })
+
+
 app.listen(3003);
