461d274e86d4728a876e54ef0911d45bce35ed3f
Vidhi Sethi <sethi.vidhi@gmail.com>
2024-12-18 12:33:12 +0530

    crud in mongoose hurray

--- 039-mongoose/01-mg.js
+++ 039-mongoose/01-mg.js
@@ -1,14 +1,57 @@
 import mongoose, { mongo, Schema } from "mongoose";
+await mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
 
-const main = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
-    const productSchema = new mongoose.Schema({
-        name: String,
-        price: Number
+const productSchema = new mongoose.Schema({
+    name: String,
+    brand: String,
+    price: Number,
+    category: String
+});
+
+const saveInDb = async () => {
+    const product = mongoose.model('products', productSchema);
+    let data = new product({
+        "name": "google pixel pro",
+        "brand": "google",
+        "price": 349,
+        "category": "mobile"
     });
-    const productModel = mongoose.model('products', productSchema);
-    let data = new productModel({"name": "m8", "price": 1000});
     let result = await data.save();
     console.log(result);
 }
-main();
+// saveInDb();
+
+const updateInDb = async () => {
+    const product = mongoose.model('products', productSchema);
+    let data = await product.updateOne(
+        {
+            "name": "note 101010"
+        },
+        {
+            $set: {
+                "name": "note 10",
+                "price": 289
+            }
+        }
+    )
+    console.log(data);
+}
+// updateInDb();
+
+const deleteInDb = async () => {
+    const product = mongoose.model('products', productSchema);
+    let data = await product.deleteMany(
+        {
+            "name": "google pixel pro"
+        }
+    )
+    console.log(data);
+}
+deleteInDb();
+
+const findInDb = async () => {
+    const product = mongoose.model('products', productSchema);
+    let data = await product.find( {name : "google pixel pro"});
+    console.log(data);
+}
+findInDb();
